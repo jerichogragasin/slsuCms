84300521aef30ed79dd20798d5bf77ee19c5f230
jerichogragasin <gragasinjericho26@gmail.com>
2022-07-24 00:46:43 +0800

    Reverted to Boostrap CDN's

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,9 +15,10 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+
     <style>
         .tree-header{
             background-color: var(--bs-body-bg);
@@ -76,6 +77,7 @@
             height: auto;
             width: auto;
         }
+
     </style>
 </head>
 <body>
