04a5033ad7922b0b84af77b0b67eb989778a1ee0
jerichogragasin <gragasinjericho26@gmail.com>
2022-07-23 22:20:57 +0800

    Added Custom Migrations
    -Moved from String to LongText DataType for Description Entry

---
+++ database/migrations/2022_07_18_082745_create_outputs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('outputs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('area_id');
+            $table->string('parameter_id');
+            $table->string('title');
+            $table->longText('description');
+            $table->string('files');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('outputs');
+    }
+};
